55095acc6d9c7e5a41bfeaab75dbcc73568c4461
Dylan Lacey <github@dylanlacey.com>
2014-06-02 20:31:39 -0700

    Add debugging

--- spec/features/the_first_spec.rb
+++ spec/features/the_first_spec.rb
@@ -1,7 +1,11 @@
 require "rails_helper"
 
 describe "The index page", sauce: true, js: true do
-  it "should be a marvellous creation" do
+
+  STDERR.puts "Inside describe block"
+  
+  it "should have some content" do
+    STDERR.puts "Inside spec"
     visit "/"
     expect(page).to have_content "This is the content we're looking for"
   end
