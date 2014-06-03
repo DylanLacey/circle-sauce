6db0b26b8719dec710b52a608acc1d32921ecd3f
Dylan Lacey <github@dylanlacey.com>
2014-06-02 21:30:09 -0700

    Output when we're beginning tests

--- spec/features/the_first_spec.rb
+++ spec/features/the_first_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 describe "The index page", sauce: true, js: true do
 
-  STDERR.puts "Inside describe block"
-  
+  STDOUT.puts "Inside describe block"
+
   it "should have some content" do
-    STDERR.puts "Inside spec"
+    STDOUT.puts "Inside spec"
     visit "/"
     expect(page).to have_content "This is the content we're looking for"
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,11 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  config.before :all do
+    STDOUT.puts "Beginning tests"
+  end
+  
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
